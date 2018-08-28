2cf2f0e7976f7db428977499895b11a775fbd8b4
f8122dac <jesse.cho@protonmail.ch>
2018-08-28 17:03:22 +0900

    Added vim-plug installation procedure

--- setup.sh
+++ setup.sh
@@ -2,6 +2,18 @@
 
 BASE_DIR=$HOME
 
+# tmux
 cp -v ./tmux/tmux.conf $BASE_DIR/.tmux.conf
+
+
+# neovim
+mkdir -p $BASE_DIR/.config/nvim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp -v ./nvim/* $BASE_DIR/.config/nvim/
+
+
+# zprezto
 cp -v ./zprezto/* $BASE_DIR/.zprezto/runcoms/
