a90e9e76b258c69bd705652c0c21179d2e0893f6
Jesse Y. Cho <jesse.cho@protonmail.ch>
2018-09-09 19:26:02 +0900

    added bspwmrc sxhkdrc rofi polybar terminator

--- setup.sh
+++ setup.sh
@@ -5,15 +5,24 @@ BASE_DIR=$HOME
 # tmux
 cp -v ./tmux/tmux.conf $BASE_DIR/.tmux.conf
 
-
 # neovim
-mkdir -p $BASE_DIR/.config/nvim
-
+cp -rv ./nvim $BASE_DIR/.config/
+# vim-plug installation: https://github.com/junegunn/vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp -v ./nvim/* $BASE_DIR/.config/nvim/
-
-
 # zprezto
 cp -v ./zprezto/* $BASE_DIR/.zprezto/runcoms/
+
+# terminator
+cp -rv ./terminator $BASE_DIR/.config/
+
+# bspwm
+cp -rv ./bspwm $BASE_DIR/.config/
+cp -rv ./sxhkd $BASE_DIR/.config/
+
+# rofi
+cp -rv ./rofi $BASE_DIR/.config/
+
+#polybar
+cp -rv ./polybar $BASE_DIR/.config/
