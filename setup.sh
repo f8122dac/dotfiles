15c14caff73e76958cdd4b922c32b62ad647ef22
Jesse Y. Cho <jesse.cho@protonmail.ch>
2018-12-19 18:45:46 -0800

    renew

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ BASE_DIR=$HOME
 cp -v ./tmux/tmux.conf $BASE_DIR/.tmux.conf
 
 # neovim
-cp -rv ./nvim $BASE_DIR/.config/
+mkdir $BASE_DIR/.config/nvim
+cp -v ./nvim/* $BASE_DIR/.config/nvim/
 # vim-plug installation: https://github.com/junegunn/vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
